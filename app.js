9beb17f6ca25077b9c9cadd1e1a3da8cf87868a5
hwidung <118262343+hwidung@users.noreply.github.com>
2023-05-21 15:28:44 +0900

    setting

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ class App {
         console.log("App Loaded");
         this.canvas = document.getElementById("myCanvas");
         this.ctx = this.canvas.getContext("2d");
-
-
         this.init();
         this.gm = null;
         this.inGame(1);
@@ -32,13 +30,44 @@ class App {
     */
     inGame(difficulty) {
         this.gm = new gameManager(this.ctx, difficulty);
+
         this.gm.startGame();
-        
+
         // test call
         console.log("inGame complete")
     }
 
-}
+    /*
+    Author : 조휘원
+    Date : 2023-05-20
+    Description : 환경설정
+
+    background change & level change
+    */
+    bindEventHandlers() {
+        $("input[name='bgd']").change(() => {
+          const selectedBackground = $("input[name='bgd']:checked").val();
+          if (selectedBackground === "bgd1") {
+            this.canvas.style.backgroundImage = "url('./images/In-Game/background1.jpg')";
+          } else if (selectedBackground === "bgd2") {
+            this.canvas.style.backgroundImage = "url('./images/In-Game/background2.jpg')";
+          }
+        });
+    
+        $("#level_1").click(() => {
+          this.inGame(1); // 초급 난이도
+        });
+    
+        $("#level_2").click(() => {
+          this.inGame(2); // 중급 난이도
+        });
+    
+        $("#level_3").click(() => {
+          this.inGame(3); // 고급 난이도
+        });
+    }
+
+}   
 
 window.onload = () => {
     new App();
